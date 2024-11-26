a16004ff3206ae8d182020392be44f6c2dba04be
Phạm Uyên <uyenpham47139@gmail.com>
2024-11-26 10:39:49 +0700

    Initialize views

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 
+import colors from 'tailwindcss/colors';
+
 export default {
   content: [
     "./views/**/*.{html,js,jsx,ts,tsx}", // Make sure it covers all your relevant files
   ],
   theme: {
     colors: {
+      colors,
       // Configure your color palette here
       blue: {
         700: '#1D4ED8',  // Example color definition for blue-700
---
+++ views/admin/Dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+        <div className="bg-colors-sky-300">
+            This is Dashboard
+        </div>
+    );
+}
+
+export default Dashboard;
