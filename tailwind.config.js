32cc1662a81446cc4c57b165ee536aca9ef7c433
uyenpham139 <uyenpham47139@gmail.com>
2024-11-27 11:05:56 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,24 @@ export default {
   theme: {
     colors: {
       colors,
+      sky: colors.sky,  // Replace lightBlue with sky
+      stone: colors.stone,  // Replace warmGray with stone
+      neutral: colors.neutral,  // Replace trueGray with neutral
+      gray: colors.gray,  // Replace coolGray with gray
+      slate: colors.slate,  // Replace blueGray with slate
       // Configure your color palette here
       blue: {
         700: '#1D4ED8',  // Example color definition for blue-700
+      },
+      green: {
+        100: '#ECFADC',
+        700: '#9FD4A3',
+      },
+      white: {
+        100: '#FFFFFF',
+      },
+      black: {
+        900: '#000000',
       },
     },
     extend: {},
